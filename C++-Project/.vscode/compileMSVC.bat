eceafa8bfcb6c9b3422aa12c222a7f6e7a88dceb
Kenvix <kenvixzure@live.com>
2019-10-26 11:02:30 +0800

    Show warnings and notes & Add MSVC C++ Complier

---
+++ C++-Project/.vscode/compileMSVC.bat
@@ -0,0 +1,6 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+
+set compilerflags=/Od /Zi /EHsc
+set linkerflags=/OUT:"%2"
+cl.exe %compilerflags% "%1" /link %linkerflags%

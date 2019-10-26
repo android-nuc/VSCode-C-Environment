d661098f96cc3132db0ed874e1e09a9b8740cf13
Kenvix <kenvixzure@live.com>
2019-10-26 16:19:22 +0800

    Better suppport for msvc

--- C++-Project/.vscode/compileMSVC.bat
+++ C++-Project/.vscode/compileMSVC.bat
@@ -1,6 +1,8 @@
 @echo off
+cd /d "%3"
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
 set compilerflags=/Od /Zi /EHsc
 set linkerflags=/OUT:"%2"
 cl.exe %compilerflags% "%1" /link %linkerflags%
+exit 0
